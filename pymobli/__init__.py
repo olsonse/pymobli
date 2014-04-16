53219c2e5f9ccc6fccb0f600bfd1b36c5266c04e
Spencer E. Olson <olsonse@umich.edu>
2014-04-16 01:45:29 -0600

    several usage fixes

--- pymobli/__init__.py
+++ pymobli/__init__.py
@@ -1,77 +1,112 @@
 #!/usr/bin/env python
-class DictObj(object):
-    def __getitem__(self, attr):
-        return getattr(self,attr,"")
+import re
 
-class Page(object):
+class Page(dict):
     def __init__(self, title=""):
+        super(Page,self).__init__()
+        self.__dict__ = self
         self.title = title
         self.header = Header(title="")
         self.content = []
         self.footer = []
 
+class DictBase(dict):
+  def __init__(self, attrib=dict(), **attrib_kwargs):
+    super(DictBase,self).__init__()
+    self.__dict__ = self
+    self.attributes = attrib.copy()
+    self.attributes.update( attrib_kwargs )
+  @property
+  def attrib(self):
+    return ' '.join([
+      '{}={}'.format(i[0],repr(i[1]))
+      for i in self.attributes.items()
+    ])
+  def dict(self, **kw):
+    D = self.copy()
+    D['attrib'] = self.attrib
+    D.update(kw)
+    return D
+
 #List types == nested,numbered,read-only,splitbutton
-class GroupBase(DictObj):
-    def __init__(self, style="", filter="false", inset="false", title=""):
-        self.style = style
-        self.title = title
-        self.items = []
-        self.filter = filter
-        self.inset = inset
-    def add(self,li):
-        self.items.append(li)
+class GroupBase(DictBase):
+  def __init__(self, style="", filter="false", inset="false", title="",
+               content='', *args, **kwargs):
+    super(GroupBase,self).__init__(*args,**kwargs)
+    self.style = style
+    self.title = title
+    self.items = (content == '' and []) or [content]
+    self.filter = filter
+    self.inset = inset
+  def add(self,li):
+    self.items.append(li)
+  def dict(self, tag='', **kw):
+    D = super(GroupBase,self).dict(**kw)
+    m = re.match('<([^>]*)>', tag)
+    if m:
+      ti,tf = '<{}>'.format(m.group(1)), '</{}>'.format(m.group(1))
+    else: ti,tf = '', ''
+    D['items'] = "\n".join(['{}{}{}'.format(ti,a,tf) for a in self.items])
+    return D
+
 
 class Header(GroupBase):
     def __repr__(self):
-        self.items = "\n".join(["%s" % a for a in self.items])
         return '''
-<div data-role="header">
+<div data-role="header" %(attrib)s>
 %(items)s
 </div>
-''' % self
+''' % self.dict()
 
 class Title(GroupBase):
-    def __repr__(self):
-        return '<h1>%(title)s</h1>' % self
+  def __init__(self, level=1, *args, **kwargs):
+    super(Title,self).__init__(*args, **kwargs)
+    self.level = level
+  def __repr__(self):
+    return '<h{level} {attrib}>{title}{items}</h{level}>'.format(**self.dict())
 
     
 class List(GroupBase):
     def __repr__(self):
-        if self.style == "numbered": self.style = "ol"
-        else: self.style = "ul"
-        self.items = "\n".join(["<li>%s</li>" % a for a in self.items])
+        style = "ul"
+        if self.style == "numbered": style = "ol"
         return '''
-<%(style)s data-role="listview" data-filter="%(filter)s" data-inset="%(inset)s">
+<%(style)s data-role="listview" data-filter="%(filter)s"
+ data-inset="%(inset)s" %(attrib)s>
 %(items)s
-</%(style)s>''' % self
+</%(style)s>''' % self.dict(tag='<li>', style=style)
 
-class Text(DictObj):
-    def __init__(self,content="", type=""):
+class Text(DictBase):
+    def __init__(self,content="", type="", *args, **kwargs):
+        super(Text,self).__init__(*args,**kwargs)
         self.content = content
         if type == "code": self.tag = "code"
-        else: self.tag = "p"
+        else:              self.tag = "p"
     def __repr__(self):
-        return "<%(tag)s>%(content)s</%(code)s>" % self
+        return "<%(tag)s %(attrib)s>%(content)s</%(tag)s>" % self.dict()
 
 class ButtonGroup(GroupBase):
     def __repr__(self):
-        self.items = "\n".join(["%s" % a for a in self.items])
         return '''
-<div data-role="controlgroup" data-type="%(style)s">
+<div data-role="controlgroup" data-type="%(style)s" %(attrib)s>
 %(items)s
-</div>''' % self
+</div>''' % self.dict()
     
 class Inline(GroupBase):
     def __repr__(self):
-        self.items = "\n".join(["%s" % a for a in self.items])
         return '''
-<div data-inline="true">
+<div data-inline="true" %(attrib)s>
 %(items)s
-</div>''' % self
+</div>''' % self.dict()
 
-class ItemBase(DictObj):
+class Div(GroupBase):
+    def __repr__(self):
+        return '<div %(attrib)s> %(items)s </div>' % self.dict()
+
+class ItemBase(DictBase):
     def __init__(self, title='', href='', transition='slide', icon="",
-                 theme="c", inline="", other=dict(), **kwargs):
+                 theme="c", inline="", *args, **kwargs):
+        super(ItemBase,self).__init__(*args, **kwargs)
         self.title = title
         self.href = href and 'href="%s"' % href or ""
         self.transition = transition and 'data-transition="%s"' % transition or ""
@@ -80,22 +115,17 @@ class ItemBase(DictObj):
         self.inline = inline and 'data-inline="%s"' % inline or ""
         if not title and icon:
             self.icon = 'data-icon="%s" data-iconpos="notext"' % icon
-        self.quickattrs = '%(href)s %(transition)s' % self
-        self._other = other
-        self._kwargs = kwargs
-    @property
-    def other(self):
-        other = dict( self._other )
-        other.update( self._kwargs )
-        return ' '.join(['{}="{}"'.format(*i) for i in other.items()])
+        self.quickattrs = '{} {}'.format(self.href, self.transition)
 
 class Link(ItemBase):
     def __repr__(self):
-        return '<a %(other)s %(href)s %(transition)s>%(title)s</a>' % self
+        return '<a %(attrib)s %(href)s %(transition)s>%(title)s</a>' \
+                % self.dict()
 
 class Button(ItemBase):
     def __repr__(self):
-        return '<a data-role="button" %(other)s %(inline)s %(icon)s %(href)s %(transition)s %(theme)s>%(title)s</a>' % self
+        return '<a data-role="button" %(attrib)s %(inline)s %(icon)s ' \
+               '%(href)s %(transition)s %(theme)s>%(title)s</a>' % self.dict()
 
 class Image(ItemBase):
     def __init__(self, src='', width='', height='', *args, **kwargs):
@@ -104,6 +134,7 @@ class Image(ItemBase):
       self.width = width and 'width="%s"' % width or ""
       self.height = height and 'height="%s"' % height or ""
 
-
     def __repr__(self):
-        return '<img data-role="image" %(other)s %(inline)s %(icon)s %(src)s %(width)s %(height)s %(transition)s %(theme)s alt="%(title)s"/>' % self
+        return '<img data-role="image" %(attrib)s %(inline)s %(icon)s ' \
+               '%(src)s %(width)s %(height)s %(transition)s %(theme)s ' \
+               'alt="%(title)s"/>' % self.dict()
